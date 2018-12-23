2affaff2b79baeed9cdbb100cf34fe8758f7437e
Mihir Maruti Kadam <mihirkadam16@gmail.com>
2018-12-23 22:03:59 +0530

    Added Code for Get Secret from Key Vault using Azure Function

---
+++ QuickFunctionApp/GetSecret.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace QuickFunctionApp
+{
+    public static class GetSecret
+    {
+        [FunctionName("GetSecret")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            if (name == null)
+            {
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                name = data?.name;
+            }
+
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+            string keyVaultURL = "https://quickkeyvault.vault.azure.net/";
+            var secret = await keyVaultClient.GetSecretAsync(keyVaultURL + name)
+                .ConfigureAwait(false);
+
+            return req.CreateResponse(HttpStatusCode.OK, "Key:" + name + "-- Value " + secret.Value);
+        }
+    }
+}
